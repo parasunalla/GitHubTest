3eca8299ba2690573be838b4583bc207eb2aa206
parasunalla <parasunalla@gmail.com>
2017-08-11 13:30:03 +0530

    second commit

--- ZGitHubTest1/WebContent/zgithubtest1/Page1.controller.js
+++ ZGitHubTest1/WebContent/zgithubtest1/Page1.controller.js
@@ -5,9 +5,9 @@ sap.ui.controller("zgithubtest1.Page1", {
 * Can be used to modify the View before it is displayed, to bind event handlers and do other one-time initialization.
 * @memberOf zgithubtest1.Page1
 */
-//	onInit: function() {
-//
-//	},
+	onInit: function() {
+
+	},
 
 /**
 * Similar to onAfterRendering, but this hook is invoked before the controller's View is re-rendered
